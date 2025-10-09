439cfaf6799397f190ecb2ddefa9019c35b856a9
jeyjunior <jeyjuniordg@gmail.com>
2025-10-09 17:56:59 -0300

    Add inimigos e itens

--- Assets/Scripts/Aula2.cs
+++ Assets/Scripts/Aula2.cs
@@ -7,11 +7,49 @@ public class Aula2 : MonoBehaviour
 {
     public SpriteRenderer sprite;
     public Transform transform;
+
     public Text textoPontos;
+    public Text textoVida;
+    
     public int Pontos = 0;
+    public int Vida = 100;
+    
     public int X = 0;
     public int Y = 0;
 
+    public GameObject Item;
+    public GameObject Inimigo;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Inimigo")
+        {
+            Vida -= 10;
+            Destroy(collision.gameObject);
+            GerarInimigo();
+        }
+        
+        if(collision.gameObject.tag == "Item")
+        {
+            Pontos++;
+            Destroy(collision.gameObject);
+            GerarItem();
+        }
+
+        AtualizarPontos();
+        AtualizarVida();
+    }
+    private void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            GerarItem();
+
+            for (int j = 0; j < 4; j++)
+                GerarInimigo();
+        }
+    }
     public void MoverParaCima()
     {
         Y++;
@@ -48,9 +86,45 @@ public class Aula2 : MonoBehaviour
         sprite.flipX = false;
         AtualizarPosicao();
     }
-
     public void AtualizarPosicao()
     {
         transform.position = new Vector3(X, Y, 0);
+    }
+    public void AtualizarPontos()
+    {
+        textoPontos.text = "Pontos: " + Pontos;
+    }
+    public void AtualizarVida()
+    {
+        textoVida.text = "Vida: " + Vida;
+    }
+
+    public void GerarInimigo()
+    {
+        int x = Random.Range(9, -9);
+        int y = Random.Range(5, -5);
+
+        if (x == X)
+            x++;
+
+        if (y == Y) 
+            y++;
+
+        Vector3 posicaoVisivel = new Vector3(x, y, 0);
+        GameObject.Instantiate(Inimigo, posicaoVisivel, Quaternion.identity);
+    }
+    public void GerarItem()
+    {
+        int x = Random.Range(9, -9);
+        int y = Random.Range(5, -5);
+
+        if (x == X)
+            x++;
+
+        if (y == Y)
+            y++;
+
+        Vector3 posicaoVisivel = new Vector3(x, y, 0);
+        GameObject.Instantiate(Item, posicaoVisivel, Quaternion.identity);
     }
 }
